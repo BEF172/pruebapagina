5bdb9f1655d8282e69ab8c3750b90b902a6e50ed
BEF172 <brian172172@gmail.com>
2023-09-04 11:08:55 -0300

    mejorado javascript

--- JS/boton.js
+++ JS/boton.js
@@ -1,25 +1,23 @@
 // Esta parte del código se encarga de mostrar u ocultar el botón de "subir" basado en la posición de desplazamiento vertical de la página.
-window.onscroll = function () {
+window.addEventListener('scroll', function () {
     // Verifica si la posición de desplazamiento vertical es mayor que 500 píxeles.
-    if (document.documentElement.scrollTop > 500) {
+    if (window.scrollY > 500) {
         // Si es mayor, agrega la clase 'show' al elemento con la clase '.boton_contenedor'.
         document.querySelector('.boton_contenedor').classList.add('show');
     } else {
         // Si es menor o igual a 500 píxeles, elimina la clase 'show' del elemento con la clase '.boton_contenedor'.
         document.querySelector('.boton_contenedor').classList.remove('show');
     }
-}
-
-// Esta parte del código maneja el desplazamiento suave cuando se hace clic en el botón.
-document.querySelector('.boton_contenedor').addEventListener('click', () => {
-    // Llama a la función 'scrollToTop' con un valor de duración de 1000 milisegundos (1 segundo) para volver arriba.
-    scrollToTop(1000); // 1000 milisegundos (1 segundo) para volver arriba
 });
 
+// Esta parte del código maneja el desplazamiento suave cuando se hace clic o toca el botón.
+document.querySelector('.boton_contenedor').addEventListener('click', scrollToTop);
+document.querySelector('.boton_contenedor').addEventListener('touchstart', scrollToTop);
+
 // Esta función realiza el desplazamiento suave hacia arriba.
-function scrollToTop(duracion) {
+function scrollToTop() {
     // Obtiene la posición actual de desplazamiento vertical.
-    const inicio = document.documentElement.scrollTop;
+    const inicio = window.scrollY;
     // Define el objetivo de desplazamiento, que es 0 (la parte superior de la página).
     const objetivo = 0;
     // Calcula la distancia que se debe recorrer desde la posición actual hasta el objetivo.
@@ -40,10 +38,10 @@ function scrollToTop(duracion) {
         // Calcula el tiempo transcurrido desde el inicio de la animación.
         const tiempoTranscurrido = tiempoActual - tiempoInicio;
         // Calcula la nueva posición de desplazamiento vertical utilizando la función de curva de aceleración.
-        document.documentElement.scrollTop = easeOutQuad(tiempoTranscurrido, inicio, distancia, duracion);
+        window.scrollTo(0, easeOutQuad(tiempoTranscurrido, inicio, distancia, 1000));
 
         // Si no se ha alcanzado la duración deseada, solicita el siguiente cuadro de animación.
-        if (tiempoTranscurrido < duracion) {
+        if (tiempoTranscurrido < 1000) {
             requestAnimationFrame(animateScroll);
         }
     }
