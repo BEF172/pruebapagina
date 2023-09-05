cd61ac8c007f5eeb8c3ef46b9c6187ed4ba28217
BEF172 <brian172172@gmail.com>
2023-09-04 21:58:28 -0300

    2.0

--- JS/boton.js
+++ JS/boton.js
@@ -1,24 +1,22 @@
-// Función que muestra u oculta el botón de "subir" basado en la posición de desplazamiento vertical de la página.
+// Función para mostrar u ocultar el botón de "subir" basado en la posición de desplazamiento vertical de la página.
 function toggleScrollButton() {
-    // Verifica si la posición de desplazamiento vertical es mayor que 500 píxeles.
     if (window.scrollY > 500 || document.documentElement.scrollTop > 500) {
-        // Si es mayor, muestra el botón.
         document.querySelector('.boton_contenedor').classList.add('show');
     } else {
-        // Si es menor o igual a 500 píxeles, oculta el botón.
         document.querySelector('.boton_contenedor').classList.remove('show');
     }
 }
 
 // Agrega un evento que se activa cuando se realiza un desplazamiento en la página.
-window.addEventListener('scroll', toggleScrollButton);
+window.addEventListener('scroll', () => {
+    toggleScrollButton();
+});
 
-// Bandera para controlar el estado de animación del botón
 let isAnimating = false;
 
-// Esta función realiza el desplazamiento suave hacia arriba cuando se hace clic en el botón.
+// Función para desplazar suavemente hacia arriba cuando se hace clic en el botón.
 function scrollToTop(duration) {
-    if (isAnimating) return; // Evita iniciar otra animación si ya está en curso
+    if (isAnimating) return;
     isAnimating = true;
 
     const start = window.scrollY || document.documentElement.scrollTop;
@@ -39,7 +37,7 @@ function scrollToTop(duration) {
         if (elapsedTime < duration) {
             requestAnimationFrame(animateScroll);
         } else {
-            isAnimating = false; // La animación ha terminado
+            isAnimating = false;
         }
     }
 
@@ -48,6 +46,67 @@ function scrollToTop(duration) {
 
 // Agrega un evento que se activa cuando se hace clic en el botón de "subir".
 document.querySelector('.boton_contenedor').addEventListener('click', () => {
-    // Llama a la función para desplazar suavemente hacia arriba.
-    scrollToTop(1000); // 1000 milisegundos (1 segundo) para volver arriba
+    scrollToTop(1000);
 });
+
+const vga = document.getElementById('vgafoto');
+
+if (vga) {
+    vga.addEventListener('click', function () {
+        const elementoDestino = document.getElementById('VGACONTENIDO');
+
+        if (elementoDestino) {
+            elementoDestino.scrollIntoView({
+                behavior: 'smooth', // Desplazamiento suave
+                block: 'center',     // Alinea la parte superior del elemento con el centro del contenedor
+            });
+
+        }
+    });
+}
+
+const dvi = document.getElementById('dvifoto');
+
+if (dvi) {
+    dvi.addEventListener('click', function () {
+        const elementoDestino = document.getElementById('DVICONTENIDO');
+
+        if (elementoDestino) {
+            elementoDestino.scrollIntoView({
+                behavior: 'smooth', // Desplazamiento suave
+                block: 'center',     // Alinea la parte superior del elemento con el centro del contenedor
+            });
+        }
+    });
+}
+
+const hdmi = document.getElementById('hdmifoto');
+
+if (hdmi) {
+    hdmi.addEventListener('click', function () {
+        const elementoDestino = document.getElementById('HDMICONTENIDO');
+
+        if (elementoDestino) {
+            elementoDestino.scrollIntoView({
+                behavior: 'smooth', // Desplazamiento suave
+                block: 'center',     // Alinea la parte superior del elemento con el centro del contenedor
+            });
+        }
+    });
+}
+
+const displayport = document.getElementById('displayportfoto');
+
+if (displayport) {
+    displayport.addEventListener('click', function () {
+        const elementoDestino = document.getElementById('DISPLAYPORTCONTENIDO');
+
+        if (elementoDestino) {
+            elementoDestino.scrollIntoView({
+                behavior: 'smooth', // Desplazamiento suave
+                block: 'center',     // Alinea la parte superior del elemento con el centro del contenedor
+            });
+
+        }
+    });
+}
